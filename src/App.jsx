ef3510eaacdf3cd7bfdde87df99b683372d42408
Ashu Singh <singhaashu271@gmail.com>
2026-04-07 22:12:34 +0530

    latest update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
 // ---------------------------------------------//
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import UserConfessionPage from './pages/UserConfessionPage';
-import AdminDashboardPage from './pages/AdminDashboardPage';
-import AdminLoginPage from './pages/AdminLoginPage';
+
 import SuccessPage from './pages/SuccessPage';
 
 export default function App() {
@@ -12,7 +11,6 @@ export default function App() {
       <Routes>
         <Route path="/" element={<UserConfessionPage />} />
         <Route path="/success" element={<SuccessPage />} />
-       
       </Routes>
     </BrowserRouter>
   );
